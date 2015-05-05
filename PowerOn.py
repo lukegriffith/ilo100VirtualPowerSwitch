80303a7abebb62f3fcaefecef4824cd69c96fd88
lukemgriffith <lukemgriffith@gmail.com>
2015-05-05 23:42:36 +0100

    formatting

--- PowerOn.py
+++ PowerOn.py
@@ -3,12 +3,12 @@ import iLoScraping
 import sys
 
 # Checks for command line parameter
-if (len(sys.argv) > 1): # if command line parameter more than
-    if (str(sys.argv[1]) == "local"): # and matches local
-        hostname = "10.0.0.05" # is set to string
+if (len(sys.argv) > 1):  # if command line parameter more than
+    if (str(sys.argv[1]) == "local"):  # and matches local
+        hostname = "10.0.0.05"  # is set to string
 
-else: # anything other
-        hostname = "iloLab.lgDns.co.uk" # set as default string
+else:  # anything other
+    hostname = "iloLab.lgDns.co.uk"  # set as default string
 
 # print(hostname)
 # executing iloScraping.py function
